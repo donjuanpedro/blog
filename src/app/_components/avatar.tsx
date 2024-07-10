9e95021fe9398c4aa2720b996cd47c5fe0264e14
Walker Lyle <walker@Walkers-Air.attlocal.net>
2024-07-10 15:28:01 -0500

    update link

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -7,7 +7,9 @@ const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
       <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
-      <div className="text-xl font-bold">{name}</div>
+      <a href="https://walkerlyle.com/">
+        <div className="text-xl font-bold">{name}</div>
+      </a>
     </div>
   );
 };
